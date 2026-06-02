89e54846f13a13374f519c62304c994762bb31c0
Nihad <nihadtaghiyev1@gmail.com>
2026-06-03 01:32:35 +0400

    cors added

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from rag import get_bot
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 bot = get_bot()
 
 class QuestionRequest(BaseModel):
